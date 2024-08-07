40a6e08278116f9233ffd95133dfffa95662096b
Dmitry Erofeev <56806136+kniksees@users.noreply.github.com>
2024-08-07 15:50:46 +0300

    Initial Commit

---
+++ ProductListSwiftUI/ProductListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListSwiftUIApp.swift
+//  ProductListSwiftUI
+//
+//  Created by Dmitry Erofeev on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProductListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
